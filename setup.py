0e0d4f747bfe68c284ff4638bd24fe062c038b10
Erwin Mintiens <erwin.mintiens@protonmail.com>
2023-06-12 20:25:06 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="whist-score",
-    version="0.0.6",
+    version="0.1.0",
     author="Erwin Mintiens",
     author_email="erwin.mintiens@protonmail.com",
     license_files=("LICENSE",),
